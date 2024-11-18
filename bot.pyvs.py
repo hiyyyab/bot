1f5ece26a318755507fd00a3e2f6f9e5a9d31121
hiyyyab <104599273+hiyyyab@users.noreply.github.com>
2024-11-17 19:20:33 -0800

    bot.pyvs.py

--- bot.pyvs.py
+++ bot.pyvs.py
@@ -2,22 +2,23 @@ import discord
 import random
 from discord.ext import commands
 
+
 intents = discord.Intents.default()
 intents.message_content = True
-
 
 client = commands.Bot(command_prefix="!",intents=intents)
 
 @client.event
-async def on_ready(self):
-        print(f'Logged on as {self.user}!')
+async def on_ready():
+        print(f'Logged on as {client.user}!')
 
-async def on_message(self, message):
+async def on_message(message):
         print(f'Message from {message.author}: {message.content}')
 
 
 
-
+#gifs
+#punch gif
 punch_gifs = ['https://media1.tenor.com/m/DznZysOV8tIAAAAd/kick-anime.gif']
 punch_names = ['Punches You!']
 
@@ -31,7 +32,46 @@ async def punch(ctx):
     embed.set_image(url=(random.choice(punch_gifs)))
     await ctx.send(embed = embed)
 
+#anya crying gif
+cry_gifs = ['https://media1.tenor.com/m/0qj0aqZ0nucAAAAd/anya-spy-x-family-anime-anya-crying.gif']
+cry_names = ['cry!']
 
+@client.command()
+async def cry(ctx):
+    embed = discord.Embed(
+        colour = (discord.Colour.random()),
+        description = f"{ctx.author.mention} {(random.choice(cry_names))}"
 
-client.run('discord token')
+    )
+    embed.set_image(url=(random.choice(cry_gifs)))
+    await ctx.send(embed = embed)
+#yay gif 
+yay_gifs = ['https://media.tenor.com/Fi5H8EfqtFAAAAAM/yay-yeah.gif']
+yay_names = ['yay!']
+
+@client.command()
+async def yay(ctx):
+    embed = discord.Embed(
+        colour = (discord.Colour.random()),
+        description = f"{ctx.author.mention} {(random.choice(yay_names))}"
+
+    )
+    embed.set_image(url=(random.choice(yay_gifs)))
+    await ctx.send(embed = embed)
+
+#titan run
+run_gifs = ['https://media1.tenor.com/m/Aznjkw6cS0MAAAAd/attack-on-titan-shingeki-no-kyojin.gif']
+run_names = ['run!']
+
+@client.command()
+async def run(ctx):
+    embed = discord.Embed(
+        colour = (discord.Colour.random()),
+        description = f"{ctx.author.mention} {(random.choice(run_names))}"
+
+    )
+    embed.set_image(url=(random.choice(run_gifs)))
+    await ctx.send(embed = embed)    
+
+client.run('discord bot token')
 
